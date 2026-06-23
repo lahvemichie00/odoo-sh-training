8e4e288529655e27fbbd84790f19e1f1508be643
lahvemichie00 <mpriscam00@gmail.com>
2026-06-23 10:40:43 +0800

    Initial Odoo custom module

---
+++ custom_addons/hello_world/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "Hello World",
+    "version": "18.0.1.0.0",
+    "summary": "Simple training module for Odoo.sh",
+    "author": "Michele Michael",
+    "category": "Training",
+    "depends": ["base"],
+    "data": [],
+    "installable": True,
+    "application": True,
+}
