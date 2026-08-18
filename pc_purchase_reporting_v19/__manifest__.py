76c58eea4a5bfc667ea9bc988425a4b9557fe909
lahvemichie00 <mpriscam00@gmail.com>
2026-08-18 15:59:50 +0800

    Add purchase order period reporting wizard

---
+++ pc_purchase_reporting_v19/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Purchase Reporting Extension",
+    "version": "19.0.1.0.0",
+    "category": "Purchase",
+    "summary": "Custom Purchase Reporting Period Export",
+    "depends": [
+        "purchase",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/purchase_order_period_views.xml",
+    ],
+    "installable": True,
+    "application": False,
+    "license": "LGPL-3",
+}
