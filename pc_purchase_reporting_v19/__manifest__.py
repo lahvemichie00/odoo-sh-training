dffa0f49e241e062d8276359bea3f3ebd1434565
lahvemichie00 <mpriscam00@gmail.com>
2026-08-18 17:05:34 +0800

    Rename purchase reporting menu to Purchase Analysis

--- pc_purchase_reporting_v19/__manifest__.py
+++ pc_purchase_reporting_v19/__manifest__.py
@@ -3,14 +3,18 @@
     "version": "19.0.1.0.0",
     "category": "Purchase",
     "summary": "Custom Purchase Reporting Period Export",
+
     "depends": [
         "purchase",
+        "purchase_enterprise",
     ],
+
     "data": [
         "security/ir.model.access.csv",
         "views/purchase_report_views.xml",
         "views/purchase_order_period_views.xml",
     ],
+
     "installable": True,
     "application": False,
     "license": "LGPL-3",
