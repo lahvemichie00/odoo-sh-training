1948b4d8d959b713d0d044b02597f5862e0c761a
lahvemichie00 <mpriscam00@gmail.com>
2026-08-19 14:38:27 +0800

    Fix purchase dashboard RPC service model

--- pc_purchase_reporting_v19/models/purchase_dashboard.py
+++ pc_purchase_reporting_v19/models/purchase_dashboard.py
@@ -1,12 +1,14 @@
-from odoo import models
+from odoo import models, api
 
 
-class PurchaseDashboard(models.Model):
+class PurchaseDashboard(models.AbstractModel):
     _name = "purchase.dashboard"
     _description = "Purchase Reporting Dashboard"
 
 
+    @api.model
     def get_purchase_dashboard_data(self):
+
         return {
             "total_amount": 866911.35,
             "untaxed_amount": 866266.19,
