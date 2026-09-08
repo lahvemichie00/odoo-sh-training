09c7afdb3565534c145ab385252021991fe0b152
lahvemichie00 <mpriscam00@gmail.com>
2026-09-08 11:53:40 +0800

    Fix FOC wizard model loading and action

--- pc_purchase_request_v19/wizard/purchase_order_foc_wizard.py
+++ pc_purchase_request_v19/wizard/purchase_order_foc_wizard.py
@@ -1,11 +1,10 @@
-from odoo import _, fields, models
-from odoo.exceptions import UserError
+from odoo import fields, models, _
 
 
 class PurchaseOrderFocWizard(models.TransientModel):
 
     _name = "purchase.order.foc.wizard"
-    _description = "Add Free Of Charge Product"
+    _description = "Add FOC Product"
 
 
     purchase_order_id = fields.Many2one(
@@ -24,80 +23,29 @@ class PurchaseOrderFocWizard(models.TransientModel):
 
     quantity = fields.Float(
         string="Quantity",
-        default=1.0,
-        required=True,
+        default=1,
     )
 
 
-    def action_add_foc_line(self):
+    def action_add_foc(self):
 
         self.ensure_one()
 
+        self.env["purchase.order.line"].create(
+            {
+                "order_id": self.purchase_order_id.id,
 
-        if not self.purchase_order_id:
+                "product_id": self.product_id.id,
 
-            raise UserError(
-                _("Purchase Order is missing.")
-            )
+                "name": self.product_id.display_name,
 
+                "product_qty": self.quantity,
 
-        if self.quantity <= 0:
+                "price_unit": 0,
 
-            raise UserError(
-                _("FOC quantity must be greater than zero.")
-            )
-
-
-        order = self.purchase_order_id
-
-
-        order.order_line.create({
-
-            "order_id":
-                order.id,
-
-
-            "product_id":
-                self.product_id.id,
-
-
-            "name":
-                "FOC - %s"
-                % self.product_id.display_name,
-
-
-            "product_qty":
-                self.quantity,
-
-
-            "product_uom":
-                self.product_id.uom_id.id,
-
-
-            "price_unit":
-                0.0,
-
-
-            "is_foc": True,
-
-
-            "taxes_id":
-                [(6, 0, [])],
-
-
-        })
-
-
-        order.message_post(
-            body=_(
-                "FOC product added: %s (%s qty)"
-            )
-            % (
-                self.product_id.display_name,
-                self.quantity,
-            )
+                "is_foc": True,
+            }
         )
-
 
         return {
             "type": "ir.actions.act_window_close"
